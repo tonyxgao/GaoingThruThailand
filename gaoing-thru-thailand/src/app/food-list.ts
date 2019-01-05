1a19342e526a5cb98c9b94a168fbb34e146e7db5
tonygao <tonygao2@illinois.edu>
2019-01-05 19:03:19 +0400

    added styling and more content

--- gaoing-thru-thailand/src/app/food-list.ts
+++ gaoing-thru-thailand/src/app/food-list.ts
@@ -1,7 +1,28 @@
-import { Food } from './food';
+import { Food } from "./food";
 
 export const FOOD: Food[] = [
-    { id: 1, name: 'Spicy Pork Noodles', review: 'Spicy, just how I like them!', date: '12-24-2018', image: '../assets/images/spicy_noods.jpg' },
-    { id: 2, name: 'Basil Chicken and Egg', review: 'Lovely, absolutely lovely', date: '12-25-2018', image: '../assets/images/basil_chicken.jpg' },
-    { id: 3, name: 'Hokkien Noodles', review: 'This was at a Mark Wiens recommended restaurant (Lock Tien) and it was delicious!', date: '12-26-2018', image: '../assets/images/hokkien_noodles.jpg' },
-  ];
+  {
+    id: 1,
+    name: "Spicy Pork Noodles",
+    review:
+      "This was our first 'real' meal in Thailand. Emily and I were mostly just walking around the area near Khao San road when we stumbled upon a tiny food stall sandwiched between two store fronts. On the way there we also picked up some egg rolls from a street food vendor. Overall, I think this meal might've cost at most $3.50. All things considered, it was definitely a good way to kick off just how good and CHEAP the food in Thailand is. While the egg rolls could've been a little fresher, the noodles themselves were spicy and had even more kick when we added some Thai chilis. Great way to start our food-ventures through Thailand!",
+    date: "12-24-2018",
+    image: "../assets/images/spicy_noods.jpg"
+  },
+  {
+    id: 2,
+    name: "Basil Chicken and Egg",
+    review:
+      "During our second day in Bangkok, Emily and I were trying to find a restaurant that one of my favorite food vloggers recommended (Mark Wiens). After walking around in circles for a while, we ultimately decided to just go to a restaurant right next door. There, we ordered basil stir-fried with a protein. I opted to also add an egg. Again, I think this meal in total was around $4 (cheap food is a very recurring thing in Thailand). The flavors were really nice. Added with the dry Thai chilis and vinegar, the flavors all worked really well. I really enjoyed this dish.",
+    date: "12-25-2018",
+    image: "../assets/images/basil_chicken.jpg"
+  },
+  {
+    id: 3,
+    name: "Hokkien Noodles",
+    review:
+      "On top of cheap food on this trip, another recurring theme is Mark Wiens. He is an absolutely awesome Youtuber based in Bangkok, but travels all around Thailand eating amazing food. This place in particular, Lock Tien was one of the spots he recommended in Phuket to try. About a 15 minute walk away from our hotel, we walked in pretty blistering weather and were sweating our butts off. It was well worth it though as the noodles, priced at less than $2 (55 baht) was delicious. It was so delicious we even had to get a second bowl!",
+    date: "12-26-2018",
+    image: "../assets/images/hokkien_noodles.jpg"
+  }
+];
