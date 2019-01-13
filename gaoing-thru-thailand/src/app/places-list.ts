62c9c85ea6e301700e9f5a3ced78841d20cc3c04
tonygao <tonygao2@illinois.edu>
2019-01-13 16:21:46 -0500

    added more content to the about page and organized images into folders

--- gaoing-thru-thailand/src/app/places-list.ts
+++ gaoing-thru-thailand/src/app/places-list.ts
@@ -7,7 +7,7 @@ export const PLACES: Place[] = [
     review:
       "If you are a backpacker, then you will have gone through this road. We went here two nights actually since our first night here was a Monday. Fun fact unknown to us is that a lot of stalls and restaurants are closed on Monday in Thailand as part of a 'street cleaning' day. This picture in particular was taking on a Monday, so things were actually pretty slow relative to what it was on Tuesday. The street is absolutely bustling with food vendors, restaurant promoters, massage promoters, and more as far as the eye can see. Primarily all tourists, it was definitely an experience to walk down. My only real regret was that we didn't get something like an alcohol bucket.",
     date: "12-24-2018",
-    image: "../assets/images/khao_san.jpg"
+    image: "../assets/images/places/khao_san.jpg"
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const PLACES: Place[] = [
     review:
       "This was the first real temple Emily and I went to. Since you have to wear pants to temples, we waited until our second day in Bangkok in the AM to stop by this place. Entry fee was a steep 500 baht ($15), but honestly I thought it was pretty well worth how massive everything was. Not to mention we went right around when it opened, so crowds were not nearly as big as I was expecting. It truly is a grand place with towering gold temples and a huge prayer shrine. Overall, would definitely recommend if you go to any temple in Bangkok to go to this one. Just make sure you follow the dress code!",
     date: "12-25-2018",
-    image: "../assets/images/grand_palace.jpg"
+    image: "../assets/images/places/grand_palace.jpg"
   },
   {
     id: 3,
@@ -23,6 +23,6 @@ export const PLACES: Place[] = [
     review:
       "We were only here for a couple hours during our second day in Phuket, but really enjoyed the beach. The place is absolutely swamped with tourists, but water and views are beautiful. Emily and I took turns laying in the sun and swimming in the water. This place is definitely worth stopping by in Phuket, but definitely beware of just how crowded it is around the area. The only regret I really have with this place was we didn't spend more time here or have the funds to go parasailing.",
     date: "12-27-2018",
-    image: "../assets/images/patong.jpg"
+    image: "../assets/images/places/patong.jpg"
   }
 ];
